3acb27d25b0e47e1a5dccca19ee34fc83f022293
Rei-li <rei-li@mail.ru>
2014-12-02 19:44:09 +0300

    Каркас для простого IoC контейнера

---
+++ GrowUp/GrowUp.IoC/RegistrationHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowUp.IoC
+{
+    /// <summary>
+    /// Регистрация типов в контейнера инверсии управления
+    /// </summary>
+    class RegistrationHelper
+    {
+        public static void Configure(Container container)
+        {
+            //container.Register<IProjectsService, Projects>();
+            //container.Register<IProjectsRepository, ProjectsData>();
+
+            //container.Register<ITasksService, Tasks>();
+            //container.Register<ITasksRepository, TaskData>();
+
+            //container.Register<IStatusService, Statuses>();
+            //container.Register<IStatusRepository, StatusData>();
+
+            //container.Register<IPersonsService, Persons>();
+            //container.Register<IPersonsRepository, PersonsData>();
+
+            //container.Register<DbHelper, DbHelper>();
+        }
+    }
+}
