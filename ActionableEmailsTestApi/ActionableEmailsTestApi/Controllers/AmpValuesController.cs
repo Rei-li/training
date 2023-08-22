63a5d1a7c8a2ee580a26e40c93af253a3129661f
Nadezda Salauyova <Nadezda.Salauyova@itechart-group.com>
2023-08-22 12:59:17 +0300

    Added amp test

---
+++ ActionableEmailsTestApi/ActionableEmailsTestApi/Controllers/AmpValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Http;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.O365.ActionableMessages.Utilities;
+
+namespace ActionableEmailsTestApi.Controllers
+{
+    public class AmpValuesController : ApiController
+    {
+        static string test = "";
+        static string requestTest = "";
+        static string cardSender = "";
+        static string actionSender = "";
+        static List<string> claims = new List<string>();
+
+        static string cardSenderString = "cardSender: {0}";
+        static string actionSenderString = "actionSender: {0}";
+
+        // GET api/values
+        public string Get()
+        {
+            return test;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public HttpResponseMessage Post([FromBody] string value)
+        {
+            HttpRequestMessage request = this.ActionContext.Request;
+
+            test = value.ToString();
+
+            
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+
+            return response;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
